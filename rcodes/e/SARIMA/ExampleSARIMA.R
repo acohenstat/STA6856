eb5745243e7be1a0200a2216a93b22afbe13a5aa
acohenstat <acohen@uwf.edu>
2023-02-21 11:35:19 -0600

    e folder removed

--- rcodes/e/SARIMA/ExampleSARIMA.R
+++
@@ -1,8 +0,0 @@
-library(fpp3)
-
-us_retail_employment <- us_employment %>%
-  filter(year(Month) >= 1990, Title == "Retail Trade") %>%
-  select(-Series_ID)
-autoplot(us_retail_employment, Employed) +
-  labs(y = "Persons (thousands)",
-       title = "Total employment in US retail")
